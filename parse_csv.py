0cceaffa1b9cb8241d844466e40d3500e6e0b1cb
Mariatta <mariatta.wijaya@gmail.com>
2016-11-12 16:35:39 -0500

    Prettify json data add upload date calculate patch age display patch age

--- parse_csv.py
+++ parse_csv.py
@@ -1,6 +1,6 @@
 import csv
 import json
-
+from datetime import datetime
 
 group_map = {
     'cpython': 0,
@@ -17,8 +17,7 @@ def parse_csv_to_json(filename):
         for index, item in enumerate(reader):
             repo_name = item['repo']
             cpython_type = repo_name == 'cpython'
-            data.append(
-                {
+            data_dict = {
                     'id': index,
                     'committer': item['committer'],
                     'issue_id': item['issue_id'],
@@ -26,11 +25,25 @@ def parse_csv_to_json(filename):
                     'start': item['commit_date'],
                     'group': group_map[repo_name],
                     'repo_name': repo_name,
-                    'cpython_type': cpython_type
+                    'cpython_type': cpython_type,
                 }
-            )
+            commit_date = datetime.strptime(item['commit_date'], '%Y-%m-%d')
+            upload_date = datetime.strptime(item['upload_date'], '%Y-%m-%d')
+            age = (commit_date.date() - upload_date.date()).days
+            if age > 0:
+                data_dict['has_age'] = True
+                if age == 1:
+                    data_dict['age'] = "{} day".format(age)
+                else:
+                    data_dict['age'] = "{} days".format(age)
+            data.append(data_dict)
     with open('./javascripts/contrib_data.js', 'w+') as file:
-        file.write("var contrib_data = {};".format(json.dumps(data)))
+        file.write("var contrib_data = {};".format(
+            json.dumps(
+                data,
+                indent=4,
+                sort_keys=True
+            )))
 
 
 if __name__ == '__main__':
